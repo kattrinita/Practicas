454f34857b1164bb77fe88d76fe6341a3ecd81dd
kattrinita <ariadnakatrinafernandez23@gmail.com>
2025-06-28 17:09:43 -0300

    Add files via upload

---
+++ libreria2/my-app/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-contenido">
+                <p>&copy; 2025 BookNest. Todos los derechos reservados.</p>
+        
+            <div className="redes">
+                <a href="#!" aria-label="Instagram"><i className="fa-brands fa-instagram"></i></a>
+                <a href="#!" aria-label="Facebook"><i className="fa-brands fa-facebook"></i></a>
+                <a href="#!" aria-label="Twitter"><i className="fa-brands fa-twitter"></i></a>
+            </div>
+
+            <a href="#!" className="terminos">Términos y condiciones</a>
+            </div>
+         </footer>
+    );
+}
---
+++ libreria2/my-app/src/components/Hero-banner.jsx
@@ -0,0 +1,18 @@
+export default function HeroBanner() {
+  return (
+    <section id="hero-banner">
+      <div id="hero-texto">
+        <h1>¡Leé más por menos!</h1>
+        <p>
+          <b>Los libros que amás con precios que también vas a amar</b>
+        </p>
+        <div id="boton">
+          <a href="#!">
+            <button>Ver Ofertas</button>
+          </a>
+        </div>
+      </div>
+      <img id="hero-imagen" src="/img/hero.png" alt="Estanteria de libros" />
+    </section>
+  );
+}
